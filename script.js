64e02398e2bfc6063c38514b6ebcaeb0b2014f6d
Teseo <danicabral15@outlook.com>
2023-04-07 16:52:33 -0300

    Fix: fuction that adds random colors

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 const $squareContainerParent = document.querySelector(
   "#square-container-parent"
 );
-
-const btnBoxSize = document.getElementById("btn-box-size");
-
-let squareBoxGlobal;
+// Buttons
+const $btnBoxSize = document.getElementById("btn-box-size"),
+  $btnBlackBox = document.getElementById("btn-black-box"),
+  $btnRainbowBox = document.getElementById("btn-rainbow-box"),
+  $btnEraser = document.getElementById("btn-eraser");
+// Flag
+let flag = "0";
+// Default color
+let colorBox = "black";
 
 // Function that adds rows and columns
 const addSquares = (column, row) => {
@@ -14,40 +19,74 @@ const addSquares = (column, row) => {
     const $squareBoxContainer = document.createElement("div");
     $squareBoxContainer.classList.add("square-box-container");
     $squareContainerParent.appendChild($squareBoxContainer);
+
     // Add rows
     for (let index = 0; index < row; index++) {
       const $squareBox = document.createElement("div");
       $squareBox.classList.add("square-box");
+      $squareBox.setAttribute("id", "square-box");
       $squareBoxContainer.appendChild($squareBox);
     }
   }
 };
 
+// Default value
 addSquares(16, 16);
 
-document.addEventListener("click", (e) => {
-  console.log("hover en", e.target);
-  if (e.target.matches(".square-box")) {
-    squareBoxGlobal = e.target;
-    squareBoxGlobal.classList.add("black");
-  }
-
-  if (e.target.matches(".btn-box-size")) {
-    let columnSize = prompt("Dame el valor 1", 16);
-    let rowSize = columnSize;
-    if (isNaN(columnSize) === true) {
-      alert("Debes ingresar un numero!");
-    } else if (columnSize > 100) {
-      alert("No se puede ingresar valores mayores a 100");
-    } else {
-      removeChildsFromParentContainer();
-      addSquares(parseInt(columnSize), parseInt(rowSize));
-    }
-  }
-});
-
+// Mientras el contendor padre tenga hijos(verificado con el metodo hasChildNodes)
+// se va borrando las viejas columnas y filas, y se agregan las nuevas que
+// introducio el usuario.
 function removeChildsFromParentContainer() {
   while ($squareContainerParent.hasChildNodes()) {
     $squareContainerParent.removeChild($squareContainerParent.firstChild);
   }
 }
+
+// Get user columns/row size
+$btnBoxSize.addEventListener("click", () => {
+  let columnSize = prompt("Dame el valor 1", 16);
+  let rowSize = columnSize;
+  if (isNaN(columnSize) === true || columnSize === null) {
+    alert("Debes ingresar un numero!");
+  } else if (columnSize > 100) {
+    alert("No se puede ingresar valores mayores a 100");
+  } else {
+    removeChildsFromParentContainer();
+    addSquares(parseInt(columnSize), parseInt(rowSize));
+  }
+});
+
+function getColor(pColor) {
+  return (colorBox = pColor);
+}
+
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+$squareContainerParent.addEventListener("mouseover", (e) => {
+  if (flag === 1) {
+    e.target.style.setProperty("background-color", getRandomColor());
+  } else {
+    e.target.style.setProperty("background-color", colorBox);
+  }
+});
+
+$btnBlackBox.addEventListener("click", () => {
+  flag = 0;
+  getColor("black");
+});
+
+$btnEraser.addEventListener("click", () => {
+  flag = 0;
+  getColor("white");
+});
+
+$btnRainbowBox.addEventListener("click", () => {
+  flag = 1;
+});
